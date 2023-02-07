8198214af0a21b417e6b13dcca7bc1fb2e0962d7
Benjamin Parsons-Willis <ben@vyrz.dev>
2023-02-07 19:41:32 +0000

    Init Chapter 3
    TESTS: PASSING

---
+++ rango/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
+
+def index(request: HttpRequest):
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about'>About</a>")
+
+
+def about(request: HttpRequest):
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
